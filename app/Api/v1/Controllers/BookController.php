b046931f78aac9ab89a3632fb39695eb6280e14f
“xushuhui” <474497097@qq.com>
2019-03-26 21:17:41 +0800

    增加图书模块

---
+++ app/Api/v1/Controllers/BookController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Api\V1\Controllers;
+
+class BookController  
+{
+    public function getBooks()
+    {
+        # code...
+    }
+    public function getBook()
+    {
+        # code...
+    }
+    public function search()
+    {
+        # code...
+    }
+    public function createBook()
+    {
+        # code...
+    }
+    public function updateBook()
+    {
+        # code...
+    }
+    public function deleteBook()
+    {
+        # code...
+    }
+    
+}
