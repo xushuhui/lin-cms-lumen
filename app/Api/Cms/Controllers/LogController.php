c959b39e63aecde65ece6195422b2cacd9fbc180
“xushuhui” <474497097@qq.com>
2019-03-26 20:53:39 +0800

    增加日志控制器和用户控制器

---
+++ app/Api/Cms/Controllers/LogController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Api\Cms\Controllers;
+
+class LogController  
+{
+    //查询所有日志
+    public function index()
+    {
+        # code...
+    }
+    //搜索日志
+    public function search()
+    {
+        # code...
+    }
+    //查询日志记录的用户
+    public function users()
+    {
+        # code...
+    }
+}
---
+++ app/Api/Cms/Controllers/UserController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Api\Cms\Controllers;
+
+class UserController extends Controller
+{
+    public function __construct()
+    {
+        //
+    }
+    //注册
+    public function register()
+    {
+        return "rests";
+    }
+    //登录
+    public function login()
+    {
+        # code...
+    }
+    //更新用户信息
+    public function update()
+    {
+        # code...
+    }
+    //修改密码
+    public function change_password()
+    {
+        # code...
+    }
+    //查询自己信息
+    public function information()
+    {
+        # code...
+    }
+    //刷新令牌
+    public function refresh()
+    {
+        # code...
+    }
+    //查询自己拥有的权限
+    public function auths()
+    {
+        # code...
+    }
+}
