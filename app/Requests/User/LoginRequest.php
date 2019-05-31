5b8478809e3c5d1a3a181d9288447c740382b959
“xushuhui <474497097@qq.com>
2019-05-31 14:28:43 +0800

    update login function

---
+++ app/Requests/User/LoginRequest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/5/31
+ * Time: 14:17
+ */
+
+namespace App\Requests\User;
+
+
+class LoginRequest
+{
+
+}
---
+++ app/Services/UserService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/5/31
+ * Time: 14:16
+ */
+
+namespace App\Services;
+
+
+class UserService
+{
+
+}
